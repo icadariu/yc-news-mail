47fc8a4bec5632dcdb7ea5e82cf3340b53b6f6ff
Claudiu Hojda <djhojd@gmail.com>
2016-07-04 18:01:00 +0300

    fetch topstories

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-//first try
+// HackerNews API - https://github.com/HackerNews/API
+
+// https://github.com/stefanpenner/es6-promise#auto-polyfill
+require('es6-promise').polyfill();
+// https://github.com/matthew-andrews/isomorphic-fetch#usage
+require('isomorphic-fetch');
+
+function jsonFetch(url) {
+  const opts = {
+    method: 'get',
+    // mode: 'cors',
+    // credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  };
+
+  return fetch(url, opts)
+    .then((response) => {
+      const { status } = response;
+      if (status >= 400) {
+        throw new Error(`Bad response from server with status ${status}`);
+      }
+      return response.json();
+    });
+}
+
+const urlHN = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
+jsonFetch(urlHN)
+  .then((obj) => console.log('response obj', obj))
+  .catch((e) => console.error(e));
