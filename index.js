7b699c97a72e44be334606b1c0b95c75b9f10e5e
Ionut Cadariu <ionut@dev.ro>
2016-07-07 22:16:19 +0300

    linter check

--- index.js
+++ index.js
@@ -29,20 +29,31 @@ function jsonFetch(url) {
 const urlHN = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
 jsonFetch(urlHN)
   .then((stories) => {
-    //const storyId = stories[0];
+    // const storyId = stories[0];
     // const storyIdLength = stories.length;
 //    storyScore = {};
-    for (var i = 0; i < stories.length; i++) {
+    let i;
+    for (i = 0; i < stories.length; i++) {
       const storyId = stories[i];
       const storyUrl = `https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`;
       jsonFetch(storyUrl)
-        .then(storyObj => console.log(storyObj.score,storyObj.title));
+        .then(storyObj => console.log(storyObj.score, storyObj.title));
+
+/*
+.then(function (selectedOption) {
+             if (selectedOption === 'Yes') {
+                 return this.save();
+             } else {
+                 Q.resolve()
+             }
+         });
+*/
 // TODO: create the if statement and show only news with more than 50 points
     //     .then(storyObj) {
     //       if (storyObj.score > 50) {
     //         storyScore.push(storyObj);
     //    };
     //  };
-   }
+    }
   })
   .catch((e) => console.error(e));
