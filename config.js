23251ba013fc8c0f9750ad0e118a0371f7c5973f
Ionut Cadariu <ionut@dev.ro>
2016-07-20 21:07:18 +0300

    better code

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.ycScore = 50; // rating > 50
+exports.time = 1000 * 60 * 60; // Program will run every hour
+// exports.time = 5000; // Program will run every hour
--- index.js
+++ index.js
@@ -4,10 +4,8 @@
 require('es6-promise').polyfill();
 // https://github.com/matthew-andrews/isomorphic-fetch#usage,
 require('isomorphic-fetch');
-// const bestStories = [];
-
-// HN_SCORE=600 node index.js
-const myScore = process.env.HN_SCORE || 50;
+// see config.js for default values
+const config = require('./config');
 
 let i;
 
@@ -37,37 +35,40 @@ const BestStoriesDB = mongoose.model('BestStories', {
   title: String,
   sent: Boolean,
 });
-
+const myScore = config.ycScore;
 const jsonFetch = require('./utils').jsonFetch;
-jsonFetch(urlHN)
+
+function newsCheck() {
+  jsonFetch(urlHN)
   .then((stories) => {
     for (i = 0; i < stories.length; i++) {
       const storyId = stories[i];
       const storyUrl = `https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`;
       jsonFetch(storyUrl)
-        .then((storyObj) => {
-          if (storyObj.score > myScore) {
-            const HNurl = `https://news.ycombinator.com/item?id=${storyObj.id}`;
-            const data = {
-              id: storyObj.id,
-              url: storyObj.url,
-              comments: storyObj.url || HNurl,
-              score: storyObj.score,
-              title: storyObj.title,
-              sent: false,
-            };
-            const news = new BestStoriesDB(data);
+      .then((storyObj) => {
+        if (storyObj.score > myScore) {
+          const HNurl = `https://news.ycombinator.com/item?id=${storyObj.id}`;
+          const data = {
+            id: storyObj.id,
+            url: storyObj.url,
+            comments: storyObj.url || HNurl,
+            score: storyObj.score,
+            title: storyObj.title,
+            sent: false,
+          };
+          const news = new BestStoriesDB(data);
 
-            BestStoriesDB.findOne({ id: storyObj.id }, function (err, obj) {
-              if (obj == null) {
-                news.save();
-              }
-            });
-          }
-        })
-        .catch((e) => console.error(e));
+          BestStoriesDB.findOne({ id: storyObj.id }, function (err, obj) {
+            if (obj == null) {
+              news.save();
+            }
+          });
+        }
+      })
+      .catch((e) => console.error(e));
     }
   })
   .catch((e) => console.error(e));
-
-// TODO: user should be able to choose story score
+}
+// program will run every hour(see config.js)
+setInterval(newsCheck, config.time);
