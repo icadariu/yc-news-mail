f446d7f84b04457d101b3bd97aada40863630248
Ionut Cadariu <ionut@dev.ro>
2016-07-20 10:52:11 +0300

    send-mail also knows about env variable now

--- send-mail.js
+++ send-mail.js
@@ -5,8 +5,19 @@ const mGunDomain = mGunCreds.mGunDomain;
 const emailAddress = mGunCreds.emailAddress;
 const mailgun = require('mailgun-js')({ apiKey: mailGunApiKey, domain: mGunDomain });
 
+let mongoUrl = '';
+
+if (process.env.NODE_ENV === 'production') {
+  mongoUrl = 'mongodb://mongo/yc';
+} else {
+  mongoUrl = 'mongodb://localhost/yc';
+}
+// db connect lines
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yc');
+mongoose.connect(mongoUrl);
+
+//const mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/yc');
 
 const BestStoriesDB = mongoose.model('BestStories', { id: String, url: String,
   comments: String, score: Number, title: String, sent: Boolean });
