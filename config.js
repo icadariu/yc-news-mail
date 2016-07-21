5b205381697477f091fdcfa5cd2c7f492f48a49c
Ionut Cadariu <icadariu@users.noreply.github.com>
2016-07-21 23:20:56 +0300

    cleanup

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 exports.ycScore = 50; // rating > 50
 exports.ycCheck = 1000 * 60 * 60; // Program will run every hour
 exports.ycMail = 1000 * 60 * 60 * 8; // send mail every 8 hours
-// exports.ycCheck = 10000;
-// exports.ycMail = 1000 * 40; // send mail every 8 hours
