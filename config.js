419ea283a4f1fea823f7eb39403acdc3987bb7fe
Ionut Cadariu <ionut@dev.ro>
2016-07-22 20:55:03 +0300

    sending news at 21:00 and 09:00

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.ycScore = 50; // rating > 50
+exports.ycScore = 60; // rating > 50
 exports.ycCheck = 1000 * 60 * 60; // Program will run every hour
-exports.ycMail = 1000 * 60 * 60 * 8; // send mail every 8 hours
+// exports.ycMail = 1000 * 60 * 60 * 8; // send mail every 8 hours
